065455d8232eda19fd51e4a62aabb9c98460154e
robbyae <robbyastrong@gmail.com>
2024-05-18 18:14:18 -0600

    added about

--- js/main.js
+++ js/main.js
@@ -735,6 +735,7 @@ function enable_sound()
     notation_btn.disabled = false;
     mode_btn.disabled = false;
     bib_btn.disabled = false;
+    about_btn.disabled = false;
     Tone.start();
     welcome_txt();
 }
@@ -822,16 +823,19 @@ var history_btn = document.getElementById("history-button");
 var notation_btn = document.getElementById("notation-button");
 var mode_btn = document.getElementById("mode-button");
 var bib_btn = document.getElementById("bibliography-button");
+var about_btn = document.getElementById("about-button");
 history_btn.disabled = true;
 notation_btn.disabled = true;
 mode_btn.disabled = true;
 bib_btn.disabled = true;
+about_btn.disabled = true;
 // var dict_display = document.getElementById("dictionary");
 var history_display = document.getElementById("history");
 var notation_display = document.getElementById("notation");
 var info_window = document.getElementById("info-window");
 var mode_display = document.getElementById("modes");
 var bib_display = document.getElementById("bibliography");
+var about_display = document.getElementById("about");
 var window_clicked = 0;
 x.addEventListener("click", () => winderp("x"));
 // dict_btn.addEventListener("click", () => winderp("dict"));
@@ -839,6 +843,7 @@ history_btn.addEventListener("click", () => winderp("history"));
 notation_btn.addEventListener("click", () => winderp("notation"));
 mode_btn.addEventListener("click", () => winderp("modes"));
 bib_btn.addEventListener("click", () => winderp("bibliography"));
+about_btn.addEventListener("click", () => winderp("about"));
 var svg_wrapper = document.getElementById("svg-wrapper");
 var prev_active;
 function winderp(active)
@@ -850,6 +855,7 @@ function winderp(active)
         notation_display.style.display = "none";
         mode_display.style.display = "none";
         bib_display.style.display = "none";
+        about_display.style.display = "none";
         document.getElementById("body-div").removeEventListener("click", hide_info);
         info_window.style.display = "block";
         x.style.display = "block";
@@ -858,6 +864,7 @@ function winderp(active)
         else if(active=="notation"){notation_display.style.display = "block";}
         else if(active=="modes"){mode_display.style.display = "block";}
         else if(active=="bibliography"){bib_display.style.display = "block";}
+        else if(active=="about"){about_display.style.display = "block";}
         svg_wrapper.style.height = info_window.offsetHeight - 400 + "px";
         window_clicked = 1;
         prev_active = active;
@@ -880,6 +887,7 @@ function winderp(active)
             notation_display.style.display = "none";
             mode_display.style.display = "none";
             bib_display.style.display = "none";
+            about_display.style.display = "none";
         }
         else
         {
@@ -899,6 +907,7 @@ function hide_info()
     notation_display.style.display = "none";
     mode_display.style.display = "none";
     bib_display.style.display = "none";
+    about_display.style.display = "none";
     document.getElementById("body-div").removeEventListener("click", hide_info);
     window_clicked = 0;
 }
